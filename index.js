7a772b17f35528af3b75af49ea6a320ee8f8f936
guna9 <osh01039@naver.com>
2021-07-23 18:39:32 +0900

    이미지추가 & 헤더 모달창 CSS 수정

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-console.log("무야호");
-
 const header = document.getElementById("header");
-const formSearchBox = header.querySelector("#header .formBox");
+const formSearchBox = header.querySelector(".formBox");
 
+const navFormBtn = document.querySelector(".formBox > div");
+const navFormBtnSib = navFormBtn.nextSibling;
 
 function seachBtnClick(){
     header.classList.remove("searchBtn");
@@ -13,9 +13,52 @@ function scrollFunc(){
     if(pageYOffset >= 70){
         header.classList.add("scroll", "searchBtn");
         formSearchBox.addEventListener("click", seachBtnClick); 
+        if(navFormBtn.className !== "active"){
+            navFormBtn.classList.remove('active');
+        }
     }else{
         header.classList.remove("scroll", "searchBtn");
     }
 }
 
 document.addEventListener('scroll', scrollFunc);
+//스크롤
+
+/*function navClick(){
+    navFormBtn.classList.add("active");
+    if(navFormBtn.className !== "active"){
+        console.log("active~");
+        
+    }
+    navFormBtnaddEventListener('click', navClick);
+}
+// navFormBtn 클릭시 모달창 //작성중
+*/
+
+
+function count(type) {
+    const resultElement = document.getElementById('result');
+
+    let number = resultElement.innerText;
+
+    if(type === 'plus'){
+        number = parseInt(number) + 1;
+    } else if(type === 'minus') {
+        number = parseInt(number) - 1;
+        if(number < 1) {
+            const minusBtn = document.getElementById("minus");
+            console.log("test");
+            
+        }
+    };
+    resultElement.innerText = number;
+};
+
+
+/*const mypageBtn = document.getElementsByClassId("mypageBtn");
+const mypageModal = mypageBtn.querySelector("#mypageModal");
+function (){
+
+    mypageBtn.addEventListener('click', )
+} 작성중
+*/

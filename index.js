a557d5a6135b8f24d14d6b31b7156edb31b72bbf
guna9 <osh01039@naver.com>
2021-08-01 18:30:56 +0900

    로그인 모달창 css수정 & modalActiveBg 위치 수정중

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ const navFormBtnSib = navFormBtn.nextSibling;
 
 function seachBtnClick(){
     header.classList.remove("searchBtn");
-    modalActiveBg.style.display = "block";   
+    modalActiveBg.classList.add("active");
 }
 
 function scrollFunc(){
     if(pageYOffset >= 70){
         header.classList.add("scroll", "searchBtn");
-        modalActiveBg.style.display = "none";
+        modalActiveBg.classList.remove("active");
         formSearchBox.addEventListener("click", seachBtnClick); 
         if(navFormBtn.className !== "active"){
             navFormBtn.classList.remove('active');
-            //navFormBtn.parentElement.classList.remove("active");
+            navFormBtn.parentElement.classList.remove("active");
         }
     }else{
         header.classList.remove("scroll", "searchBtn");
     }
-}
+} //-------------------20210801 modalActvieBg : header scroll일때 아닐때 순서 다시잡아야함
 
 document.addEventListener('scroll', scrollFunc);
 //스크롤 --- e
@@ -31,12 +31,10 @@ document.addEventListener('scroll', scrollFunc);
 //footer까지 스크롤이 내려가면 mobileFixedTab은 아래방향으로 사라짐
 //스크롤 모바일 --- e
 
+
 const formBtn = document.querySelectorAll('.formBtn');
 const formBox = document.querySelector('.formBox');
 const popups = document.querySelectorAll('.popup');
-
-const mypageBtn = document.getElementById('mypageBtn');
-const mapageModal = mypageBtn.querySelector("#mypageModal");
 
 formBtn.forEach(
     function(button,index){
@@ -44,16 +42,47 @@ formBtn.forEach(
         button.addEventListener('click', () => {
             formBtn[newIndex].classList.add("active"); 
             formBox.classList.add('active');
+            console.log("hey");
+            modalActiveBg.classList.remove("active");
         });
         //다른 버튼을 클릭하면 해당모달 active 클래스 지움
     }
 );
 
-function handleClick(){
-	mapageModal.classList.toggle('active');
-}
-mypageBtn.addEventListener("click", handleClick);  //마이페이지 모달 위치 다시 잡는 중 20210731
 
+const mypageBtn = document.getElementById('mypageBtn');
+const mypageModal = document.getElementById("mypageModal");
+
+function handleClick(){
+	mypageModal.classList.toggle('active');
+}
+mypageBtn.addEventListener("click", handleClick);
+//마이페이지 모달창 오픈
+
+
+const loginModalBtn = mypageModal.querySelectorAll(".loginListBtn");
+const loginModal = document.getElementById("loginModal");
+
+function loginModalClick(){
+    loginModal.classList.add('active');
+    modalActiveBg.classList.add('active');
+    document.body.style.overflow = "hidden";
+
+    const closeModalBtn = loginModal.querySelector(".closeBtn");
+    function closeBtnClick(){
+        loginModal.classList.remove("active");
+        modalActiveBg.classList.remove('active');
+        document.body.style.overflow = "auto";
+    }
+    closeModalBtn.addEventListener("click", closeBtnClick);
+    modalActiveBg.addEventListener("click", closeBtnClick);
+}
+
+for(let i = 0; i < loginModalBtn.length; i ++){
+    const item = loginModalBtn.item(i);
+    item.addEventListener("click", loginModalClick);
+} //loginListBtn 동일한 클래스선택자에게 이벤트를 줌
+//마이페이지-로그인 모달창 오픈
 
 
 function count(type) {

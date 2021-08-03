09b53dd5c76f06b3abce53c8edae5ccc71b468b3
guna9 <osh01039@naver.com>
2021-08-03 19:13:36 +0900

    체크인아웃에 prev next버튼 누르면 바뀌는 이번달과 다음달 캘린더 추가

--- index.js
+++ index.js
@@ -6,42 +6,30 @@ const modalActiveBg = document.getElementById("modalActiveBg");
 const navFormBtn = document.querySelector(".formBox > div");
 const navFormBtnSib = navFormBtn.nextSibling;
 
-function seachBtnClick(){
-    header.classList.remove("searchBtn");
-    modalActiveBg.classList.add("active");
-    if(header.className !== "scroll"){
-        console.log("not scroll");
-        modalActiveBg.classList.remove("active");
-    }
-    
-}
+const formBtn = document.querySelectorAll('.formBtn');
+const formBox = document.querySelector('.formBox');
+const popups = document.querySelectorAll('.popup');
+
+const mypageBtn = document.getElementById('mypageBtn');
+const mypageModal = document.getElementById("mypageModal");
 
 function scrollFunc(){
     if(pageYOffset >= 70){
         header.classList.add("scroll", "searchBtn");
         modalActiveBg.classList.remove("active");
+        
         formSearchBox.addEventListener("click", seachBtnClick); 
         if(navFormBtn.className !== "active"){
             navFormBtn.classList.remove('active');
             navFormBtn.parentElement.classList.remove("active");
         }
+        mypageModal.classList.remove("active");
     }else{
         header.classList.remove("scroll", "searchBtn");
     }
 }
-
 document.addEventListener('scroll', scrollFunc);
-//스크롤 --- e
-//--------------------------20210802 특정 높이에서 modalActiveBg.active가 안잡힘 ;; whyrano
-
-
-//footer까지 스크롤이 내려가면 mobileFixedTab은 아래방향으로 사라짐
-//스크롤 모바일 --- e
-
-
-const formBtn = document.querySelectorAll('.formBtn');
-const formBox = document.querySelector('.formBox');
-const popups = document.querySelectorAll('.popup');
+//스크롤 --- e  20210803 스크롤시 헤더에 있는 모든 active클래스를 지우면 어케될까요 ?? 내일 하고 집에 가자 이제^^..
 
 formBtn.forEach(
     function(button,index){
@@ -55,15 +43,21 @@ formBtn.forEach(
     }
 );
 
-const mypageBtn = document.getElementById('mypageBtn');
-const mypageModal = document.getElementById("mypageModal");
-
-function handleClick(){
-	mypageModal.classList.toggle('active');
+function seachBtnClick(){
+    header.classList.remove("searchBtn");
+    modalActiveBg.classList.add("active");
+    if(header.className !== "scroll"){
+        console.log("not scroll");
+        modalActiveBg.classList.remove("active");
+    }
+    formBtn.classList.remove("active");
 }
 
-mypageBtn.addEventListener("click", handleClick);
+mypageBtn.addEventListener("click", () => {
+    mypageModal.classList.toggle('active');
+});
 //마이페이지 모달창 오픈
+
 
 /*window.onclick = function(event){
     if (event.target !== mypageModal){
@@ -76,7 +70,7 @@ mypageBtn.addEventListener("click", handleClick);
 
 const loginModalBtn = mypageModal.querySelectorAll(".loginListBtn");
 const loginModal = document.getElementById("loginModal");
-
+const closeModalBtn = loginModal.querySelector(".closeBtn");
 
 for(let i = 0; i < loginModalBtn.length; i ++){
     const item = loginModalBtn.item(i);
@@ -87,8 +81,6 @@ for(let i = 0; i < loginModalBtn.length; i ++){
         modalActiveBg.style.zIndex = "4";
         modalActiveBg.style.display = "block";
         
-
-        const closeModalBtn = loginModal.querySelector(".closeBtn");
         closeModalBtn.addEventListener("click", () => {
             console.log("active");
             modalActiveBg.style.zIndex = "3";
@@ -100,8 +92,6 @@ for(let i = 0; i < loginModalBtn.length; i ++){
     });
 } //loginListBtn 동일한 클래스선택자에게 이벤트를 줌
 //마이페이지-로그인 모달창 오픈
-
-
 
 
 function count(type) {
@@ -122,18 +112,11 @@ function count(type) {
     resultElement.innerText = number;
 };
 
-
-/*const mypageBtn = document.getElementsByClassId("mypageBtn");
-const mypageModal = mypageBtn.querySelector("#mypageModal");
-function (){
-
-    mypageBtn.addEventListener('click', )
-} 작성중
-*/
-
+const mobileFixedTab = document.getElementById("mobileFixedTab");
+const mobileLoginBtn = mobileFixedTab.querySelector(".loginBtn");
 
 //-----------반응형
-const mql = window.matchMedia("screen and (max-width: 743px)");
+const mql = window.matchMedia("screen and (max-width: 744px)");
 if(mql.matches){
     document.querySelector(".main p").innerHTML = `에어비앤비가<br />여행지를 찾아드릴게요!`;
     const mobileSearch = document.querySelector(".mobileSearch");
@@ -149,6 +132,20 @@ if(mql.matches){
         
         });
     });
+
+    mobileLoginBtn.addEventListener("click", () => {
+        loginModal.classList.toggle('active');
+        document.body.style.overflow = "hidden";
+        
+        closeModalBtn.addEventListener("click", () => {
+            loginModal.classList.remove("active");
+            document.body.style.overflow = "auto";
+        });
+        mobileLoginBtn.addEventListener("click", () => {
+            document.body.style.overflow = "auto";
+        });
+    })//모바일 하단 fixed tab 에 로그인버튼과 모바일사이즈 로그인모달
+
 };
 
 
@@ -160,8 +157,8 @@ $(window).scroll(function(){
     const height_win = $(window).height();
 
     if(Math.round($(window).scrollTop()) == $(document).height() - $(window).height()){
-        document.getElementById("mobileFixedTab").style.bottom = "-70px";
+        mobileFixedTab.style.bottom = "-70px";
     } else {
-        document.getElementById("mobileFixedTab").style.bottom = "0";
+        mobileFixedTab.style.bottom = "0";
     }
 })
